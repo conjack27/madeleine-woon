6e5c3e640fbb2093fffcd675974618b2c5001314
Connor Graham <Connor.Graham@poort80.nl>
2020-05-08 14:41:26 +0200

    initial commit

---
+++ src/templates/Post.vue
@@ -0,0 +1,48 @@
+<template>
+    <Layout>
+        <div class="markdown-body" v-html="$page.post.content"></div>
+    </Layout>
+</template>
+
+<page-query>
+query Post ($path: String!) {
+    post: post (path: $path) {
+        title
+        content
+    }
+}
+</page-query>
+
+
+
+
+<script>
+export default {
+    metaInfo() {
+        return {
+            title: this.$page.post.title
+        }
+    }    
+}
+</script>
+
+<style>
+
+@import url("https://cdnjs.cloudflare.com/ajax/libs/github-markdown-css/4.0.0/github-markdown.min.css");
+
+
+.markdown-body {
+  box-sizing: border-box;
+  min-width: 200px;
+  max-width: 980px;
+  margin: 0 auto;
+  padding: 45px;
+}
+
+@media (max-width: 767px) {
+  .markdown-body {
+    padding: 15px;
+  }
+}
+
+</style>
