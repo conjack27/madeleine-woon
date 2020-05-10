35c3ba14a856d030058ef8d909b8446cf3314937
Connor Graham <Connor.Graham@poort80.nl>
2020-05-10 16:27:27 +0200

    changed date format

--- src/pages/Articles.vue
+++ src/pages/Articles.vue
@@ -3,7 +3,7 @@
    <div class="flex flex-col">
      <table class="w-full flex flex-row flex-no-wrap overflow-hidden my-5 border-b">
 			<thead class="border-b border-t">
-				<tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
+				<tr  class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0">
 					<th class="p-3 text-left">Title</th>
 					<th class="p-3 text-left">Publication</th>
 					<th class="p-3 text-left" width="110px">Date</th>
@@ -25,31 +25,16 @@
               </tr>
 			</thead>
 			<tbody class="flex-1 sm:flex-none">
-				<tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 border-b">
-					<td class="p-4">John Covv</td>
-					<td class="p-4 truncate">contato@johncovv.com</td>
-					<td class="p-4 text-red-400">Delete</td>
+				<tr v-for="post in $page.posts.edges" :key="post.id" class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 border-b relative">
+					<td class="p-4"><g-link class="block w-full h-full" :to="post.node.path">{{ post.node.title }}</g-link></td>
+					<td class="p-4"><g-link class="block w-full h-full" :to="post.node.path">{{ post.node.publication }}</g-link></td>
+					<td class="p-4"><g-link class="block w-full h-full" :to="post.node.path">{{ post.node.date }}</g-link></td>
 				</tr>
-				<tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 border-b">
-					<td class="p-4">Michael Jackson</td>
-					<td class="p-4 truncate">m_jackson@mail.com</td>
-					<td class="p-4 text-red-400">Delete</td>
-				</tr>
-                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 border-b">
-                    <td class="p-4">Julia</td>
-                    <td class="p-4 truncate">julia@mail.com</td>
-                    <td class="p-4 text-red-400">Delete</td>
-                </tr>
-                <tr class="flex flex-col flex-no wrap sm:table-row mb-2 sm:mb-0 border-b">
-                  <td class="p-4">Martin Madrazo</td>
-                  <td class="p-4 truncate">martin.madrazo@mail.com</td>
-                  <td class="p-4 text-red-400">Delete</td>
-                </tr>
 			</tbody>
 		</table>
-</div>
+    </div>
 
- <ul>
+    <ul>
       <li v-for="post in $page.posts.edges" :key="post.id">
         <g-link :to="post.node.path"> {{ post.node.title }} </g-link>
       </li>
@@ -91,8 +76,4 @@ export default {
       display: none;
     }
   }
-
-  td:not(:last-child) {
-  }
-
 </style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: [],
+  purge: [
+    './src/**/*.html',
+    './src/**/*.vue'
+  ],
   theme: {
     colors: {
       indigo: '#5c6ac4',
