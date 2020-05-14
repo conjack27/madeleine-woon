957cbbbbf93d8940f41a2a3d0da230b567b94645
Connor Graham <Connor.Graham@poort80.nl>
2020-05-14 16:10:47 +0200

    fix color binding

--- src/templates/Editorial.vue
+++ src/templates/Editorial.vue
@@ -1,6 +1,6 @@
 <template>
-    <Layout style='{ background-color: $page.editorial.title }'>
-        <div class="layout container mx-auto">
+    <Layout :style="{ 'background-color': $page.editorial.color }">
+        <div  class="layout container mx-auto">
             <div class="max-w-screen-md mb-8 mt-8 container mx-auto font-body article">
                 <h1 class="text-4xl text-center mb-8 font-bold">{{ $page.editorial.title }}</h1>
                 <div class="text-xl" v-html="$page.editorial.content"></div>
@@ -27,7 +27,12 @@ export default {
         return {
             title: this.$page.editorial.title
         }
-    }    
+    },
+    data() {
+        return {
+            bgColor: '#000000'
+        }
+    }
 }
 </script>
 
