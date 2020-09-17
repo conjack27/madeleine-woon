cfe27bcc146795c4605ed24e9e9c0c08f8dae069
Connor Graham <Connor.Graham@poort80.nl>
2020-09-17 19:02:12 +0200

    s

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -2,15 +2,23 @@
   <Layout class="min-h-screen home">
     <div class="layout container mx-auto pl-4 pr-4 md:pl-0 md:pr-0">
       <flickity class="h-md md:h-lg" ref="flickity" :options="flickityOptions">
-        <div v-for="carouselItem in $page.carouselItem.edges" :key="carouselItem.id" class="carousel-cell w-full items-center flex justify-between relative flex-col md:flex-row">
+        <div
+          v-for="carouselItem in $page.carouselItem.edges"
+          :key="carouselItem.id"
+          class="carousel-cell w-full items-center flex justify-between relative flex-col md:flex-row"
+        >
           <a :href="carouselItem.node.link" class="absolute w-full h-full"></a>
           <div class="carousel__title mb-8 md:mb-0">
             <h1 class="text-2xl md:text-4xl lg:text-6xl">
-              <i>{{ carouselItem.node.publication }}, </i>
-              <span>{{ carouselItem.node.title }}</span>
+              <span>{{ carouselItem.node.title }}&nbsp;-</span>&nbsp;
+              <i class="text-xl md:text-3xl lg:text-5xl">{{ carouselItem.node.publication }}</i>
             </h1>
           </div>
-          <img class="w-3/4 md:w-1/2 lg:w-5/12 h-auto" :src="carouselItem.node.image" alt="alternative text">
+          <img
+            class="w-3/4 md:w-1/2 lg:w-5/12 h-auto"
+            :src="carouselItem.node.image"
+            alt="alternative text"
+          />
         </div>
       </flickity>
     </div>
@@ -35,13 +43,12 @@
 </page-query>
 
 <script>
-import Flickity from 'vue-flickity';
+import Flickity from "vue-flickity";
 export default {
-   components: { 
-    Flickity
+  components: {
+    Flickity,
   },
-  created() {
-  },
+  created() {},
   data() {
     return {
       flickityOptions: {
@@ -49,27 +56,26 @@ export default {
         pageDots: true,
         wrapAround: true,
         autoPlay: 5000,
-        setGallerySize: false
-      }
-    }
+        setGallerySize: false,
+      },
+    };
   },
-  
+
   methods: {
     next() {
       this.$refs.flickity.next();
     },
-    
+
     previous() {
       this.$refs.flickity.previous();
-    }
+    },
   },
-  metaInfo: {
-  },
-}
+  metaInfo: {},
+};
 </script>
 
 <style>
 .home {
-	background: linear-gradient(90deg, #F2D0C9 70%, #FCF4CF 30%);
+  background: linear-gradient(90deg, #f2d0c9 70%, #fcf4cf 30%);
 }
 </style>
